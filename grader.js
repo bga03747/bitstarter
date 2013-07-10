e0694167ce2a34dd8e968d75fe6c13e73eeae255
bga03747 <brianandaliza@gmail.com>
2013-07-10 16:25:03 +0800

    test

--- grader.js
+++ grader.js
@@ -1,10 +1,35 @@
 #!/usr/bin/env node
 
+//require
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
+
+//default
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "http://guarded-fjord-5524.herokuapp.com";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+
+/*rest.get('http://google.com').on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    sys.puts(result);
+  }
+});
+*/
+var assertUrlExists = function(url) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); 
+    }
+    return instr;
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,7 +69,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Site Url', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
