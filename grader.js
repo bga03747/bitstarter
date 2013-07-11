57648623a58090999e9f8420f25c2d85c0b562b4
bga03747 <brianandaliza@gmail.com>
2013-07-11 17:57:54 +0800

    test

--- grader.js
+++ grader.js
@@ -75,7 +75,7 @@ if(require.main == module) {
         .version('0.0.1')
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'Site Url', clone(assertUrlExists), URL_DEFAULT)
+        .option('-u, --url <url>', 'Site Url', clone(assertFileExists), URL_DEFAULT)
         .parse(process.argv);
 
     var checkJson = checkHtmlFile(program.file, program.checks);
