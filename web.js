1caad819fcc3bcccb4a0331bf103ac559f366c4b
bga03747 <brianandaliza@gmail.com>
2013-07-24 11:12:23 +0800

    test

--- web.js
+++ web.js
@@ -9,12 +9,12 @@ app.get('/', function(request, response) {
   
   var a = fs.readFileSync("index.html","utf-8"); 
 
-  var buf = new Buffer(a, 'utf8');
+  //var buf = new Buffer(a, 'utf8');
 
-  len = buf.write(a,0);
+  //len = buf.write(a,0);
 
-  response.send(buf.toString('utf8', 0, len));
-  //response.send(a);
+  //response.send(buf.toString('utf8', 0, len));
+  response.send(a);
 
 });
 
